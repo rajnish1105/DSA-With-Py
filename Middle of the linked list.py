6375763c47b960c3cc0bfd0ef977f4ee1dd4a884
rajnish coder <rajnishkumar6054@gmail.com>
2026-02-16 12:18:54 +0530

    Add files via upload

---
+++ Middle of the linked list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        sp = fp = head
+        while fp and fp.next:
+            sp = sp.next
+            fp = fp.next.next
+        return sp
+        
